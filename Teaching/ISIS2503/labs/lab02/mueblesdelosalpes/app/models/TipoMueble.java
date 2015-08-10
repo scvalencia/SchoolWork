3e50e5eae12c3d10be9822b7256dd03d885c4db8
scvalencia <sc.valencia606@uniandes.edu.co>
2015-08-10 08:32:26 -0500

    TipoMueble model update, comments and binding

--- Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/TipoMueble.java
+++ Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/TipoMueble.java
@@ -1,9 +1,39 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by scvalencia606 on 7/29/15.
  */
 
 public enum TipoMueble {
-    Interior, Exterior
+    Interior,
+    Exterior;
+
+    private static Map<String, TipoMueble> namesMap = new HashMap<String, TipoMueble>(2);
+
+    static {
+        namesMap.put("interior", Interior);
+        namesMap.put("exterior", Exterior);
+    }
+
+    @JsonCreator
+    public static TipoMueble forValue(String value) {
+        return namesMap.get(StringUtils.lowerCase(value));
+    }
+
+    @JsonValue
+    public String toValue() {
+        for (Map.Entry<String, TipoMueble> entry : namesMap.entrySet()) {
+            if (entry.getValue() == this)
+                return entry.getKey();
+        }
+
+        return null;
+    }
 }
