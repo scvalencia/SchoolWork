3b943e19ec27a3833e4131cc0ae97da5a4a61da5
scvalencia <sc.valencia606@uniandes.edu.co>
2015-08-10 08:32:09 -0500

    TipoDocumento model update, comments and binding

--- Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/TipoDocumento.java
+++ Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/TipoDocumento.java
@@ -1,10 +1,38 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.lang3.StringUtils;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by scvalencia606 on 7/31/15.
  */
 
 public enum TipoDocumento {
     CC,
-    TarjetaIdentidad
+    TarjetaIdentidad;
+
+    private static Map<String, TipoDocumento> namesMap = new HashMap<String, TipoDocumento>(2);
+
+    static {
+        namesMap.put("cc", CC);
+        namesMap.put("ti", TarjetaIdentidad);
+    }
+
+    @JsonCreator
+    public static TipoDocumento forValue(String value) {
+        return namesMap.get(StringUtils.lowerCase(value));
+    }
+
+    @JsonValue
+    public String toValue() {
+        for (Map.Entry<String, TipoDocumento> entry : namesMap.entrySet()) {
+            if (entry.getValue() == this)
+                return entry.getKey();
+        }
+
+        return null;
+    }
 }
