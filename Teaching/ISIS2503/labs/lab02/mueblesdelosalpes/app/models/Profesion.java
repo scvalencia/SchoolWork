b7d753f99b8a463c941320be07de966960997161
scvalencia <sc.valencia606@uniandes.edu.co>
2015-08-10 08:31:49 -0500

    Profesion model update, comments and binding

--- Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/Profesion.java
+++ Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/Profesion.java
@@ -1,5 +1,12 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by scvalencia606 on 7/31/15.
  */
@@ -12,5 +19,33 @@ public enum Profesion {
     Economista,
     Estudiante,
     Médico,
-    Ingeniero
+    Ingeniero;
+
+    private static Map<String, Profesion> namesMap = new HashMap<String, Profesion>(8);
+
+    static {
+        namesMap.put("abogado", Abogado);
+        namesMap.put("arquitecto", Arquitecto);
+        namesMap.put("administrador", Administrador);
+        namesMap.put("diseñador", Diseñador);
+        namesMap.put("economista", Economista);
+        namesMap.put("estudiante", Estudiante);
+        namesMap.put("médico", Médico);
+        namesMap.put("ingeniero", Ingeniero);
+    }
+
+    @JsonCreator
+    public static Profesion forValue(String value) {
+        return namesMap.get(StringUtils.lowerCase(value));
+    }
+
+    @JsonValue
+    public String toValue() {
+        for (Map.Entry<String, Profesion> entry : namesMap.entrySet()) {
+            if (entry.getValue() == this)
+                return entry.getKey();
+        }
+
+        return null;
+    }
 }
