2aa19df7e46ffff62a5fd58d97e375d36c1ef4d9
scvalencia <sc.valencia606@uniandes.edu.co>
2015-08-10 15:50:18 -0500

    Model update, comments, and stuff: RegistroVenta

--- Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/RegistroVenta.java
+++ Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/models/RegistroVenta.java
@@ -1,3 +1,14 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * $Id$ RegistroVenta.java
+ * Universidad de los Andes (Bogotá - Colombia)
+ * Departamento de Ingeniería de Sistemas y Computación
+ * Licenciado bajo el esquema Academic Free License version 3.0
+ *
+ * Ejercicio: Muebles de los Alpes
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 package models;
 
 import com.avaje.ebean.Model;
@@ -5,25 +16,60 @@ import javax.persistence.Entity;
 import java.util.Date;
 
 /**
+ * Clase que modela un registro de venta realizado por un cliente
  * Created by scvalencia606 on 7/31/15.
  */
 
 @Entity
 public class RegistroVenta extends Model {
 
+    //-----------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------
+
+    /**
+     * Fecha en la que se vendió el producto
+     */
     private Date fechaVenta;
 
+    /**
+     * Producto vendido
+     */
     private Mueble producto;
 
+    /**
+     * Cantidad vendida del producto
+     */
     private int cantidad;
 
+    /**
+     * Ciudad en la que se vendió el producto
+     */
     private String ciudad;
 
+    /**
+     * Usuario que compró el producto
+     */
     private Usuario comprador;
 
+    //-----------------------------------------------------------
+    // Constructores
+    //-----------------------------------------------------------
+
+    /**
+     * Constructor sin argumentos
+     */
     public RegistroVenta() {
     }
 
+    /**
+     * Constructor de la clase con argumentos
+     * @param fechaVenta Fecha en que se realizó la venta
+     * @param producto Mueble adquirido
+     * @param cantidad Cantidad adquirida
+     * @param ciudad Ciudad en la que se vendió el producto
+     * @param comprador Usuario que compro el mueble
+     */
     public RegistroVenta(Date fechaVenta, Mueble producto, int cantidad, String ciudad, Usuario comprador) {
         this.fechaVenta = fechaVenta;
         this.producto = producto;
@@ -32,42 +78,86 @@ public class RegistroVenta extends Model {
         this.comprador = comprador;
     }
 
+    //-----------------------------------------------------------
+    // Getters y setters
+    //-----------------------------------------------------------
+
+    /**
+     * Devuelve la fecha en que se vendió el mueble
+     * @return fechaVenta Fecha de venta del mueble
+     */
     public Date getFechaVenta() {
         return fechaVenta;
     }
 
+    /**
+     * Modifica la fecha en que se vendió el mueble
+     * @param fechaVenta Nueva fecha de venta
+     */
     public void setFechaVenta(Date fechaVenta) {
         this.fechaVenta = fechaVenta;
     }
 
+    /**
+     * Devuelve el mueble adquirido
+     * @return producto Mueble adquirido
+     */
     public Mueble getProducto() {
         return producto;
     }
 
+    /**
+     * Modifica el mueble adquirido
+     * @param producto Nuevo mueble
+     */
     public void setProducto(Mueble producto) {
         this.producto = producto;
     }
 
+    /**
+     * Devuelve la cantidad de producto vendido
+     * @return cantidad Cantidad de producto vendido
+     */
     public int getCantidad() {
         return cantidad;
     }
 
+    /**
+     * Modifica la cantidad de muebles adquiridos
+     * @param cantidad Nueva cantidad de muebles
+     */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 
+    /**
+     * Devuelve la ciudad en dónde se realizó la venta
+     * @return ciudad Ciudad
+     */
     public String getCiudad() {
         return ciudad;
     }
 
+    /**
+     * Modifica la ciudad dónde se realizó la venta
+     * @param ciudad Nueva ciudad
+     */
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
 
+    /**
+     * Devuelve el usuario que realizó la compra
+     * @return comprador Usuario que realizó la compra
+     */
     public Usuario getComprador() {
         return comprador;
     }
 
+    /**
+     * Modifica el usuario que realizó la compra
+     * @param comprador Nuevo usuario
+     */
     public void setComprador(Usuario comprador) {
         this.comprador = comprador;
     }
