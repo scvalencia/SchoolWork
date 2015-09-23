5027917824185591b065720274161759cc4ed1e4
scvalencia <sc.valencia606@uniandes.edu.co>
2015-09-22 22:59:41 -0500

    A simple Python script for automata testing

--- Courses/2015-20/Theory of computation/homeworks/Q3/aux.py
+++ Courses/2015-20/Theory of computation/homeworks/Q3/aux.py
@@ -1,17 +1,19 @@
-word = 'bbbcbbb*acc*'
+word = 'baa*cbb*acc*'
 n = len(word)
 
 i = 0
+j = 0
 while i < n:
-	if i % 3 == 0:
+	if j % 3 == 0:
 		if word[i] == 'b': print 'a',
 		elif word[i] == 'c': print 'b',
 		elif word[i] == 'a': print 'c',
-	elif i % 3 == 1 and word[i + 1] == word[i]:
+	elif j % 3 == 1 and word[i + 1] == word[i]:
 		print word[i],
 		i = i + 1
-	elif i % 3 == 2:
+	elif j % 3 == 2:
 		if word[i] == '*': print word[i - 1],
 		else: print word[i],
 
 	i += 1
+	j += 1
