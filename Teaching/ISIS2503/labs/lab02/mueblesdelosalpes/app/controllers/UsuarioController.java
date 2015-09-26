676c0f8399316c19782c0d9d38643fb349446d58
scvalencia <sc.valencia606@uniandes.edu.co>
2015-09-25 21:38:54 -0500

    Donload a file from Play

--- Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/controllers/UsuarioController.java
+++ Teaching/ISIS2503/labs/lab02/mueblesdelosalpes/app/controllers/UsuarioController.java
@@ -6,6 +6,10 @@ import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -13,8 +17,33 @@ import java.util.List;
  */
 public class UsuarioController extends Controller {
 
-    public Result read() {
+    public Result read() throws IOException {
         List<Usuario> usuarios = new Model.Finder(String.class, Usuario.class).all();
+
+        try {
+
+            String content = "This is the content to write into file";
+
+            File file = new File("files/ughhh.txt");
+
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+
+            System.out.println("Done");
+            return ok(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return ok(Json.toJson(usuarios));
+
     }
 }
